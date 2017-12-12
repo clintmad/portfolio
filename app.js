271fd878683d3de06d77ed1883db58cf8fb9a6a5
Clint <clintmad21@hotmail.com>
2017-12-12 04:30:52 -0700

    Added public folder, and new project

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // catch 404 and forward to error handler
